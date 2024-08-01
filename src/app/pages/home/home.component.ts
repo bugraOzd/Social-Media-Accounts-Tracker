a6a22080bbc4e6ebda462fb197088d560fd76770
bugraOzd <bugraozdilek@gmail.com>
2024-08-01 20:13:14 +0300

    home component updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { matFilterAlt, matSearch } from '@ng-icons/material-icons/baseline';
+import { TableComponent } from '../../components/table/table.component';
+import { ModalComponent } from '../../components/modal/modal.component';
+import { SocialMediaFormComponent } from '../../components/social-media-form/social-media-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent, TableComponent, ModalComponent, SocialMediaFormComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  viewProviders: [provideIcons({ matFilterAlt, matSearch })]
 })
 export class HomeComponent {
+  isAddModalVisible = false;
 
+  onClickAddNewAccout() {
+    this.isAddModalVisible = true;
+  }
+
+  onClickCloseModal() {
+    this.isAddModalVisible = false;
+  }
 }
