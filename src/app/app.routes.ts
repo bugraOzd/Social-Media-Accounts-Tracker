be83ea89840d0f8c89faa161eb347c5af477878a
bugraOzd <bugraozdilek@gmail.com>
2024-08-01 20:03:51 +0300

    update router with empty pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { CompetitionSoftwareComponent } from './pages/competition-software/competition-software.component';
+import { WordNinjaComponent } from './pages/word-ninja/word-ninja.component';
+import { WordPyramidsComponent } from './pages/word-pyramids/word-pyramids.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'competition-software',
+    component: CompetitionSoftwareComponent
+  },
+  {
+    path: 'word-ninja',
+    component: WordNinjaComponent
+  },
+  {
+    path: 'word-pyramids',
+    component: WordPyramidsComponent
+  },
 ];
