9539be490c273ea8a8b0b25fc17a3bf63c70f73a
bugraOzd <bugraozdilek@gmail.com>
2024-08-01 20:01:04 +0300

    feature/create empty pages

---
+++ src/app/pages/word-pyramids/word-pyramids.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-pyramids',
+  standalone: true,
+  imports: [],
+  templateUrl: './word-pyramids.component.html',
+  styleUrl: './word-pyramids.component.css'
+})
+export class WordPyramidsComponent {
+
+}
