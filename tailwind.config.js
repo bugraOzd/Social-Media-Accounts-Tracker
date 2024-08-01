c19e9507ff666dd7b8d9ee3f6a81a48ffcbb7334
bugraOzd <bugraozdilek@gmail.com>
2024-08-01 20:13:29 +0300

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple:{
+          1: '#744BFC',
+          2: '#F1F4FF'
+        }
+      },
+      borderRadius: {
+        'c-lg': '39px',
+        'c-sm': '8px'
+      }
+    },
   },
   plugins: [],
 }
